54079ba10b192564361d05c32a1615193617625b
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2023-10-17 18:21:07 +0330

    Update core-ktx library

--- showcase/shared/build.gradle.kts
+++ showcase/shared/build.gradle.kts
@@ -41,7 +41,7 @@ kotlin {
             dependencies {
                 api("androidx.activity:activity-compose:1.8.0")
                 api("androidx.appcompat:appcompat:1.6.1")
-                api("androidx.core:core-ktx:1.10.1")
+                api("androidx.core:core-ktx:1.12.0")
             }
         }
         val desktopMain by getting {
