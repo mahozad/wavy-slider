3decda365f7a685c0b62e19b04b38e44b7421bbd
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2023-10-17 18:20:41 +0330

    Update activity-compose library

--- showcase/shared/build.gradle.kts
+++ showcase/shared/build.gradle.kts
@@ -39,7 +39,7 @@ kotlin {
         }
         val androidMain by getting {
             dependencies {
-                api("androidx.activity:activity-compose:1.7.2")
+                api("androidx.activity:activity-compose:1.8.0")
                 api("androidx.appcompat:appcompat:1.6.1")
                 api("androidx.core:core-ktx:1.10.1")
             }
