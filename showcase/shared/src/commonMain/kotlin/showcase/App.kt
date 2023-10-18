648f1d58150ae108c2551635046da1948732614e
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2023-10-18 12:27:04 +0330

    Add minor refactoring

--- showcase/shared/src/commonMain/kotlin/showcase/App.kt
+++ showcase/shared/src/commonMain/kotlin/showcase/App.kt
@@ -5,7 +5,7 @@ import ir.mahozad.multiplatform.material.WavySlider
 
 @Composable
 fun App() {
-    var value by remember { mutableStateOf(0.67f) }
+    var value by remember { mutableFloatStateOf(0.67f) }
     WavySlider(
         value = value,
         onValueChange = { value = it }
