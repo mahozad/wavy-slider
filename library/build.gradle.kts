3bf798029e812970250b33e9b4d66340427db7be
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2023-10-19 17:51:35 +0330

    Release version 0.2.0

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -20,7 +20,7 @@ buildscript {
 }
 
 group = "ir.mahozad.multiplatform"
-version = "0.0.1"
+version = "0.2.0"
 
 kotlin {
     // See https://central.sonatype.com/namespace/org.jetbrains.compose.material
