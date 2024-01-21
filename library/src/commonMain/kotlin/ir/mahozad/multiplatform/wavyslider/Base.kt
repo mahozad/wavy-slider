6c79903175ca8113e650af9f068326d8e79ce6c7
mahozad@work <mdihosseinzadeh@gmail.com>
2024-01-21 14:16:31 +0330

    Rename Common.kt to Base.kt and move code in Utilities.kt to it
    Also, remove the Utilities.kt file

--- library/src/commonMain/kotlin/ir/mahozad/multiplatform/wavyslider/Base.kt
+++ library/src/commonMain/kotlin/ir/mahozad/multiplatform/wavyslider/Base.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.isActive
 import kotlin.math.PI
+import kotlin.math.abs
 import kotlin.math.sin
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
@@ -203,3 +204,27 @@ private inline fun DrawScope.drawTrackActivePart(
         )
     )
 }
+
+internal fun snapValueToTick(
+    current: Float,
+    tickFractions: FloatArray,
+    minPx: Float,
+    maxPx: Float
+): Float {
+    // target is a closest anchor to the `current`, if exists
+    return tickFractions
+        .minByOrNull { abs(lerp(minPx, maxPx, it) - current) }
+        ?.run { lerp(minPx, maxPx, this) }
+        ?: current
+}
+
+// Scale x1 from a1..b1 range to a2..b2 range
+internal fun scale(a1: Float, b1: Float, x1: Float, a2: Float, b2: Float) =
+    lerp(a2, b2, calcFraction(a1, b1, x1))
+
+internal fun lerp(start: Float, stop: Float, fraction: Float) =
+    (start * (1 - fraction) + stop * fraction)
+
+// Calculate the 0..1 fraction that `pos` value represents between `a` and `b`
+internal fun calcFraction(a: Float, b: Float, pos: Float) =
+    (if (b - a == 0f) 0f else (pos - a) / (b - a)).coerceIn(0f, 1f)
