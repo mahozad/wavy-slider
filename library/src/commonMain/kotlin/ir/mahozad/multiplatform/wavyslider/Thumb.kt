f87ee417bcee0080d1a7bc86e0b76c23a2ecd9c4
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-01-24 16:23:05 +0330

    WIP

--- library/src/commonMain/kotlin/ir/mahozad/multiplatform/wavyslider/Thumb.kt
+++ library/src/commonMain/kotlin/ir/mahozad/multiplatform/wavyslider/Thumb.kt
@@ -8,45 +8,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-sealed class WavySliderThumb {
-
-    internal abstract val drawing: @Composable () -> Unit
-
-    open class Diamond(
-        val width: Dp,
-        val height: Dp = width,
-        val roundness: Dp = 0.dp
-    ) : WavySliderThumb() {
-        override fun toString() = "WavySliderThumb.Diamond"
-        companion object : Diamond(width = 24.dp, height = 24.dp, roundness = 2.dp)
-        override val drawing = @Composable {
-            Box(modifier = Modifier.width(width).height(height).background(Color.Red))
+class WavySliderThumb {
+    companion object {
+        @Composable fun Custom(content: @Composable () -> Unit) = content
+        val Diamond: @Composable () -> Unit = Diamond(24.dp)
+        fun Diamond(width: Dp, height: Dp = width): @Composable () -> Unit {
+            val f: @Composable () -> Unit = {
+                Box(modifier = Modifier.width(width).height(height).background(Color.Red))
+            }
+            return f
         }
     }
-
-    open class Pill(
-        val width: Dp,
-        val height: Dp = width * 4,
-        val roundness: Dp = 0.dp
-    ) : WavySliderThumb() {
-        override fun toString() = "WavySliderThumb.Pill"
-        companion object : Pill(width = 4.dp, height = 20.dp, roundness = 8.dp)
-        override val drawing = @Composable {
-            Box(modifier = Modifier.width(width).height(height).background(Color.Red))
-        }
-    }
-
-    data class Custom(override val drawing: @Composable () -> Unit) : WavySliderThumb()
 }
 
 @Composable
-fun ExampleComponent(thumb: WavySliderThumb) {
-    thumb.drawing()
-    when (thumb) {
-        is WavySliderThumb.Pill -> println("Pill")
-        is WavySliderThumb.Diamond -> println("Diamond")
-        is WavySliderThumb.Custom -> println("Custom")
-    }
+fun ExampleComponent(thumb: @Composable () -> Unit) {
+    thumb()
 }
 
 @Composable
