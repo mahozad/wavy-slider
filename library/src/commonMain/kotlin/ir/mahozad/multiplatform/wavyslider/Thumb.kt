690f8a8aa7956d97d89a16bd3b8576da452b4542
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-01-24 17:03:15 +0330

    WIP

--- library/src/commonMain/kotlin/ir/mahozad/multiplatform/wavyslider/Thumb.kt
+++ library/src/commonMain/kotlin/ir/mahozad/multiplatform/wavyslider/Thumb.kt
@@ -1,19 +1,26 @@
 package ir.mahozad.multiplatform.wavyslider
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.SliderPositions
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import ir.mahozad.multiplatform.wavyslider.material3.WavySlider
 
 class WavySliderThumb {
     companion object {
-        @Composable fun Custom(content: @Composable () -> Unit) = content
-        val Diamond: @Composable () -> Unit = Diamond(24.dp)
-        fun Diamond(width: Dp, height: Dp = width): @Composable () -> Unit {
-            val f: @Composable () -> Unit = {
+        @Composable fun Custom(content: @Composable (SliderPositions) -> Unit) = content
+        val Diamond: @Composable (SliderPositions) -> Unit = Diamond(24.dp)
+        fun Diamond(width: Dp, height: Dp = width): @Composable (SliderPositions) -> Unit {
+            val f: @Composable (SliderPositions) -> Unit = {
                 Box(modifier = Modifier.width(width).height(height).background(Color.Red))
             }
             return f
@@ -21,18 +28,11 @@ class WavySliderThumb {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ExampleComponent(thumb: @Composable () -> Unit) {
-    thumb()
-}
-
-@Composable
-fun ExampleCaller() {
-    Column {
-        ExampleComponent(thumb = WavySliderThumb.Diamond)
-        ExampleComponent(thumb = WavySliderThumb.Diamond(10.dp))
-        ExampleComponent(thumb = WavySliderThumb.Custom {
-            Box(modifier = Modifier.size(14.dp).background(Color.Green))
-        })
-    }
+fun Examples() {
+    WavySlider(0.5f, {}, thumb = { SliderDefaults.Thumb(remember { MutableInteractionSource() }) })
+    WavySlider(0.5f, {}, thumb = WavySliderThumb.Diamond)
+    WavySlider(0.5f, {}, thumb = WavySliderThumb.Diamond(8.dp, 24.dp))
+    WavySlider(0.5f, {}, thumb = WavySliderThumb.Custom { Text("XYX") })
 }
