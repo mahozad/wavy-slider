8253f916a68d46b848745d3083482ca5496ec6fc
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-02-14 16:33:44 +0330

    Release version 1.0.0

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -22,7 +22,7 @@ buildscript {
 }
 
 group = "ir.mahozad.multiplatform"
-version = "1.0.0-rc"
+version = "1.0.0"
 
 // See https://central.sonatype.com/namespace/org.jetbrains.compose.material
 // for the targets that Compose Multiplatform supports
