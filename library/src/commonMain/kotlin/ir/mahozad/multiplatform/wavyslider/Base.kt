4932203e0786bf9f8a26a5aa2e33047be36bfba4
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-03-15 07:41:45 +0330

    Remove an unused function

--- library/src/commonMain/kotlin/ir/mahozad/multiplatform/wavyslider/Base.kt
+++ library/src/commonMain/kotlin/ir/mahozad/multiplatform/wavyslider/Base.kt
@@ -264,19 +264,6 @@ private inline fun DrawScope.createWavyPath(
     }
 }
 
-internal inline fun snapValueToTick(
-    current: Float,
-    tickFractions: FloatArray,
-    minPx: Float,
-    maxPx: Float
-): Float {
-    // target is a closest anchor to the `current`, if exists
-    return tickFractions
-        .minByOrNull { abs(lerp(minPx, maxPx, it) - current) }
-        ?.run { lerp(minPx, maxPx, this) }
-        ?: current
-}
-
 // Scale x1 from a1..b1 range to a2..b2 range
 internal inline fun scale(a1: Float, b1: Float, x1: Float, a2: Float, b2: Float) =
     lerp(a2, b2, calcFraction(a1, b1, x1))
