6bcaa46cf2ca69054b99d2539829387f2a0587d2
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-06-03 20:54:01 +0330

    Update the app icon of the demo apps

--- showcase/desktopApp/src/jvmMain/kotlin/showcase/main.kt
+++ showcase/desktopApp/src/jvmMain/kotlin/showcase/main.kt
@@ -1,11 +1,13 @@
 package showcase
 
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
     Window(
         title = "Wavy slider showcase",
+        icon = painterResource("logo.svg"),
         onCloseRequest = ::exitApplication
     ) {
         MainView()
