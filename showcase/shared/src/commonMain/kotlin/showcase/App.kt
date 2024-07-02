4a850c4f5002a579aace0303182e504e54772bf1
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2024-07-02 16:47:31 +0330

    WIP: Update to Compose Multiplatform 1.7.0

--- showcase/shared/src/commonMain/kotlin/showcase/App.kt
+++ showcase/shared/src/commonMain/kotlin/showcase/App.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Slider as Slider2
 import androidx.compose.material3.Slider as Slider3
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SliderState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,18 +17,19 @@ import androidx.compose.material3.SliderDefaults as SliderDefaults3
 import ir.mahozad.multiplatform.wavyslider.material.WavySlider as WavySlider2
 import ir.mahozad.multiplatform.wavyslider.material3.WavySlider as WavySlider3
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
-    var value by remember { mutableFloatStateOf(0.67f) }
+    val state = remember { SliderState(0.67f) }
     Column {
-        Slider2(value = value, onValueChange = { value = it })
+        Slider2(value = state.value, onValueChange = { state.value = it })
         WavySlider2(
-            value = value,
-            onValueChange = { value = it }
+            value = state.value,
+            onValueChange = { state.value = it }
         )
         WavySlider2(
-            value = value,
-            onValueChange = { value = it },
+            value = state.value,
+            onValueChange = { state.value = it },
             waveLength = 12.dp,
             waveHeight = 24.dp,
             waveVelocity = 14.dp to RIGHT,
@@ -36,21 +39,17 @@ fun App() {
             colors = SliderDefaults2.colors(activeTrackColor = Color.Red)
         )
         WavySlider2(
-            value = value,
-            onValueChange = { value = it },
+            value = state.value,
+            onValueChange = { state.value = it },
             waveHeight = 0.dp
         )
 
         Divider(Modifier.padding(vertical = 32.dp))
 
-        Slider3(value = value, onValueChange = { value = it })
+        Slider3(state)
+        WavySlider3(state)
         WavySlider3(
-            value = value,
-            onValueChange = { value = it }
-        )
-        WavySlider3(
-            value = value,
-            onValueChange = { value = it },
+            state = state,
             waveLength = 12.dp,
             waveHeight = 24.dp,
             waveVelocity = 14.dp to RIGHT,
@@ -59,10 +58,6 @@ fun App() {
             incremental = true,
             colors = SliderDefaults3.colors(activeTrackColor = Color.Red)
         )
-        WavySlider3(
-            value = value,
-            onValueChange = { value = it },
-            waveHeight = 0.dp
-        )
+        WavySlider3(state = state, waveHeight = 0.dp)
     }
 }
